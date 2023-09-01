0dc979454c90d9511c8ce916350a1218e26edc19
Luis7123 <88041788+Luis7123@users.noreply.github.com>
2023-09-01 06:49:25 -0500

    Update CasosPrueba.py test_amortization

--- CasosPrueba.py
+++ CasosPrueba.py
@@ -89,6 +89,19 @@ class CreditCardTest(unittest.TestCase):
             self.assertEqual( resultado, 0 )  # Forzar fallo caso
         except  pago.plazoNegativo  :
             pass
+
+    def testAmortizacion(self):
+        compra = 850000
+        tasa = 3.4
+        plazo = 24
+        amortizacion = 33225.109
+        abono=90000
+        mes_abono =  5
+        df_amortization = pago.dataframe_amortizacion( compra, tasa, plazo ,abono, mes_abono )
+        
+        list_df_amortization = list(df_amortization.iloc[1])
+        list_correct_values=[52.377 , 28.900 , 23.477 ,826.522]
+        self.assert_frame_equal(pd.DataFrame(list_correct_values),pd.DataFrame(list_df_amortization))
         
 
 if __name__ == '__main__':
